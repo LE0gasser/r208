21de93687249eed800668e7277f154116f2de440
e2500616 <leo.gasser682006@gmail.com>
2026-03-11 14:57:28 +0100

    push

--- ex2.py
+++ ex2.py
@@ -1,13 +1,15 @@
 import csv
+import sys
+from pathLib import Path
+f_csv =Path(sys.argv[1])
+ip= sys.argv[3]
 
-with open('ip_log.csv' mode='r') as file
-while True:
-    try:
-        adresse_ip = int(input("entrer l'adresse que vous souhaitez rechercher"))
-        fichier = csv.reader(file)
-        if adresse_ip in file:
-            print("trouver")
+compteur_ip = 0
 
+with open(f_cssv, 'r', encoding='utf-8') as fichier.csv:
+    lignes_csv = csv.reader(fichier_csv)
+    for ligne in ligne.csv:
+        if ip in ligne:
+            compteur_ip += 1
 
-    except Exception as e:
-        print(type(e))
+print(f"Nombre d'occurence de l' @ip : {ip} : {compteur_ip}")
